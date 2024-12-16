014d54791c8ba284eea63d850895804205c34be8
Amit Singh <amitsingh5198@gmail.com>
2024-12-16 15:33:20 +0530

    Revert temp changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@ import('./src/server/env.mjs');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    ppr: 'incremental',
-  },
   env: {
     NEXT_PUBLIC_ENV: process.env.VERCEL_ENV || 'development',
   },
